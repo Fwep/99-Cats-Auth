04bac078a5febd121843c503684824f556e11606
Taran Cacacho <tarancacacho@gmail.com>
2019-07-10 19:59:24 -0700

    Write methods to check for a current user, login a user, and redirect from sign up/log in forms if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  
+  def current_user
+    user = User.find_by(session_token: session[:session_token])
+    user.nil? ? nil : user
+  end
+
+  def login_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    redirect_to cats_url
+  end
+
+  def redirect_if_logged_in
+    redirect_to cats_url if current_user
+  end
 end
