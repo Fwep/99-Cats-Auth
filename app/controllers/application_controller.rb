0517377f2b38b7d340f5adb0a138697c7c30db18
Taran Cacacho <tarancacacho@gmail.com>
2019-07-11 20:57:35 -0700

    Add cat specific logic to verify cat ownerership and user ability to approve

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :user_can_approve
   
   def current_user
-    user = User.find_by(session_token: session[:session_token])
-    user.nil? ? nil : user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def login_user!(user)
@@ -14,5 +14,17 @@ class ApplicationController < ActionController::Base
 
   def redirect_if_logged_in
     redirect_to cats_url if current_user
+  end
+
+  def verify_cat_owner
+    cats = current_user.cats.where(id: params[:id])
+
+    redirect_to cats_url if cats.empty?
+  end
+
+  def user_can_approve
+    cat_id = params[:id]
+    cat_ids = current_user.cats.pluck(:id)
+    cat_ids.include?(cat_id.to_i)
   end
 end
